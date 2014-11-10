02827d26238439ca603138cd87f2ebd516ae4f70
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2014-11-10 19:18:28 +0100

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject foo-bar/lein-template "0.1.0-SNAPSHOT"
+(defproject foo-bar/lein-template "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/magomimmo/foo-bar-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
